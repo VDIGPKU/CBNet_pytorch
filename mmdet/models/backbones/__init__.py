b2aaa8c0fdb160f47e18bb1dd64b2708e6b3054d
VDIGPKU <62875783+VDIGPKU@users.noreply.github.com>
2020-04-07 13:22:20 +0800

    Update __init__.py

--- mmdet/models/backbones/__init__.py
+++ mmdet/models/backbones/__init__.py
@@ -1,6 +1,10 @@
 from .hrnet import HRNet
 from .resnet import ResNet, make_res_layer
+from .db_resnet import DB_ResNet, db_make_res_layer
+from .tb_resnet import TB_ResNet, tb_make_res_layer
 from .resnext import ResNeXt
+from .db_resnext import DB_ResNeXt
+from .tb_resnext import TB_ResNeXt
 from .ssd_vgg import SSDVGG
 
-__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet']
+__all__ = ['ResNet', 'make_res_layer', 'ResNeXt', 'SSDVGG', 'HRNet','DB_ResNet','DB_ResNeXt','TB_ResNet','TB_ResNeXt']
